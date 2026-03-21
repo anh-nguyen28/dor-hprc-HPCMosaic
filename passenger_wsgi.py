681c208b0e6c12ebb7386997d157cbf12835d68f
Vu Ha Anh Nguyen <u.vn344255@alogin1.cluster>
2026-03-21 08:39:47 -0500

    updated dataclasses for flask issue

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,15 @@
+import os
+import sys
+
+# Activate the virtual environment
+venv_path = os.path.join(os.path.dirname(__file__), 'venv')
+site_packages = os.path.join(venv_path, 'lib', 'python3.6', 'site-packages')  # replace 3.X with your version
+sys.path.insert(0, site_packages)
+
 from app import app
 
 application = app
 
 if __name__ == '__main__':
     application.run()
+
